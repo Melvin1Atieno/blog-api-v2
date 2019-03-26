f69cde2a5062abc4188f5210aa746b90d154b957
Melvin1Atieno <melvinatieno@gmail.com>
2019-03-26 07:03:01 +0300

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :blogs
-  resources :users
+  resources :users do
+    resources :blogs
+  end
+  resources :blogs do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
